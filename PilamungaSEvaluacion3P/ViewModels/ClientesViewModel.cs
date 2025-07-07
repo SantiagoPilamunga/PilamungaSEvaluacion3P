3947b9c66ea5cf466549fe2ed0bcd254adfa072d
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-07-07 08:01:49 -0500

    Creamos ClientesPage y el view model

---
+++ PilamungaSEvaluacion3P/ViewModels/ClientesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PilamungaSEvaluacion3P.Models;
+using PilamungaSEvaluacion3P.Repository;
+
+namespace PilamungaSEvaluacion3P.ViewModels
+{
+    public class ClientesViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Cliente> Clientes { get; set; } = new();
+
+        private ClienteRepository repo;
+
+        public ClientesViewModel()
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "clientes.db3");
+            repo = new ClienteRepository(dbPath);
+
+            _ = LoadClientesAsync();
+        }
+
+        private async Task LoadClientesAsync()
+        {
+            var lista = await repo.GetClientesAsync();
+            Clientes.Clear();
+            foreach (var cliente in lista)
+                Clientes.Add(cliente);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ PilamungaSEvaluacion3P/Views/ClientesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PilamungaSEvaluacion3P.Views;
+
+public partial class ClientesPage : ContentPage
+{
+	public ClientesPage()
+	{
+		InitializeComponent();
+	}
+}
