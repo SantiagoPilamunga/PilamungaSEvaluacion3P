eb5e01b5a075df597f5ff539f98758417e91330b
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-07-07 07:29:30 -0500

    Creamos la clase cliente

---
+++ PilamungaSEvaluacion3P/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PilamungaSEvaluacion3P.Models
+{
+    public class Cliente
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Empresa { get; set; }
+        public int AntiguedadMeses { get; set; }
+        public bool Activo { get; set; }
+    }
+}
+
