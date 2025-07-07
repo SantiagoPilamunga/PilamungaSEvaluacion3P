8b0be4f1345c8ac49c6f9e4975b9584d81765419
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-07-07 07:56:15 -0500

    Cambios en AppShell

--- PilamungaSEvaluacion3P/AppShell.xaml.cs
+++ PilamungaSEvaluacion3P/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace PilamungaSEvaluacion3P
+﻿using PilamungaSEvaluacion3P.Views;
+
+namespace PilamungaSEvaluacion3P
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(RegistroPage), typeof(RegistroPage));
+
         }
     }
 }
